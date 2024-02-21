6feeddc19e1ad1a80e1edab8f89e36aa98b784b0
pw-64 <64085841+pw-64@users.noreply.github.com>
2024-02-21 11:57:32 +0000

    Cross-browser fixes - Updated `collection.html` with new js files - Fixed order of scripts in `popup.html` - Fixed `storage.js` getter & setter returns
    `popup.js`
    - fixed cross-browser check for collection tabs detection
    - fixed `Storage.set` as it is async so tabs weren't being saved before the window was closed

--- source/common/storage.js
+++ source/common/storage.js
@@ -1,9 +1,5 @@
 // for interfacing with the extension storage
 class Storage {
-    static set(key, value) {chrome.storage.local.set({[key]:value});}
-    static async get(key) {
-        let value;
-        await chrome.storage.local.get(key).then((json) => {value = json[key];});
-        return value;
-    }
+    static async set(key, value) {return await vendor.storage.local.set({[key]:value});}
+    static async get(key) {return await vendor.storage.local.get(key).then(json => json[key]);}
 }
