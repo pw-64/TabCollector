7136146d5ed0470a79dd65cd75a6611ba5bb6e3d
pw-64 <64085841+pw-64@users.noreply.github.com>
2024-02-21 09:33:37 +0000

    Added scripts to build and load the extension into a browser temporarily for testing

---
+++ testing/chrome.sh
@@ -0,0 +1,3 @@
+cd ../building
+bash 1-build.sh
+"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --load-extension="build/chrome"
---
+++ testing/firefox.sh
@@ -0,0 +1,4 @@
+cd ../building
+bash 1-build.sh
+cd build/firefox
+web-ext run --devtools
